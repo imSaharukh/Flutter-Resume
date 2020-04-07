98fbc30633fd8fe93f7369bfbd116ad52e6199c1
imSaharukh <sasaharukh@gmail.com>
2020-04-08 00:39:03 +0600

    update data

--- lib/data.dart
+++ lib/data.dart
@@ -1,17 +1,17 @@
 class Data {
-  static const WEB_TITLE = "Nam - Mobile Developer";
-  static const NAME = "Nam Do";
-  static const PHONE_NUMBER = "+84766261102";
-  static const EMAIL = "thanhnamitit@gmail.com";
-  static const LOCATION = "Hanoi, Vietnam";
-  static const BIRTHDAY = "24 February, 1995";
+  static const WEB_TITLE = "S A Saharukh - Mobile Developer";
+  static const NAME = "S A Saharukh";
+  static const PHONE_NUMBER = "+8801750729276";
+  static const EMAIL = "mail@saharukh.com";
+  static const LOCATION = "Dhaka, Bangladesh";
+  static const BIRTHDAY = "01 July";
 
   static const AVATAR = "image/avatar.png";
 
-  static const FACEBOOK_URL = "https://www.facebook.com/thanhnamitit";
-  static const INSTAGRAM_URL = "https://www.instagram.com/thanhnamitit/";
-  static const GITHUB_URL = "https://github.com/thanhnamitit";
-  static const LINKEDIN_URL = "https://www.linkedin.com/in/đỗ-nam-b51829167/";
+  static const FACEBOOK_URL = "https://www.facebook.com/imSaharukh";
+  static const INSTAGRAM_URL = "https://www.instagram.com/imSaharukh/";
+  static const GITHUB_URL = "https://github.com/imSaharukh";
+  static const LINKEDIN_URL = "https://www.linkedin.com/in/imSaharukh/";
 
   static const HOME_SENTENCE =
       "The proper use of comments is to compensate for our failure to express ourself in code.";
@@ -27,6 +27,5 @@ class Data {
   static const MESSAGE_API =
       "https://us-central1-workout-e1b53.cloudfunctions.net/api/message";
 
-  static const CV_URL =
-      "https://www.topcv.vn/download-cv?cv_id=ee5ba82f207c20f1cc1d4f562df05f25&t=1586189535";
+  static const CV_URL = "#";
 }
